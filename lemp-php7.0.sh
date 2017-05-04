09f7a51788099c3294f4efb0e25adf311b009f3c
piwik-la <admin@piwik.la>
2017-05-04 23:45:42 +0800

    Update lemp-php7.0.sh

--- lemp-php7.0.sh
+++ lemp-php7.0.sh
@@ -20,7 +20,7 @@ chkconfig nginx on
 service nginx restart
 
 apt-get update
-apt-get install -y php7.0 php7.0-cli php7.0-common php7.0-curl php7.0-fpm php7.0-gd php7.0-json php7.0-mysql php7.0-readline
+apt-get install -y php7.0 php7.0-cli php7.0-common php7.0-curl php7.0-fpm php7.0-gd php7.0-json php7.0-mysql php7.0-readline php7.0-mbstring
 apt-get -y install mariadb-server
 
 sed -i "s/^;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/" /etc/php/7.0/fpm/php.ini
