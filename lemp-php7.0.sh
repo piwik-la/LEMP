2a499d73d806c06e17e96af02b60d999df611ef9
piwik-la <admin@piwik.la>
2017-05-04 23:58:22 +0800

    Update lemp-php7.0.sh

--- lemp-php7.0.sh
+++ lemp-php7.0.sh
@@ -2,6 +2,7 @@ sudo add-apt-repository ppa:ondrej/php
 sudo apt-get install software-properties-common
 sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082a1bb943db
 sudo add-apt-repository 'deb [arch=amd64,i386,ppc64el] http://sfo1.mirrors.digitalocean.com/mariadb/repo/10.1/ubuntu trusty main'
+sudo add-apt-repository ppa:certbot/certbot
 apt-get update && apt-get upgrade -y
 apt-get install git screen make libpcre3 libpcre3-dev openssl libssl-dev linux-kernel-headers build-essential sysv-rc-conf -y
 cd /usr/src/
@@ -22,6 +23,8 @@ service nginx restart
 apt-get update
 apt-get install -y php7.0 php7.0-cli php7.0-common php7.0-curl php7.0-fpm php7.0-gd php7.0-json php7.0-mysql php7.0-readline php7.0-mbstring
 apt-get -y install mariadb-server
+sudo apt-get install certbot
+certbot certonly --webroot -w /var/www/example.com -d example.com -d www.example.com
 
 sed -i "s/^;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/" /etc/php/7.0/fpm/php.ini
 sed -i "s/^;listen.owner = www-data/listen.owner = www-data/" /etc/php/7.0/fpm/pool.d/www.conf
